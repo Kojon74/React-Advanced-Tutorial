9a9431a7071e06d3fd3071d81b23733d212960e3
Ken Johnson <dragonkenkj@gmail.com>
2021-01-12 20:26:09 -0500

    finished useEffect

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Setup from "./tutorial/1-useState/setup/5-useState-counter";
+import Setup from "./tutorial/3-conditional-rendering/setup/1-multiple-returns";
 import Final from "./tutorial/1-useState/final/1-error-example";
 
 function App() {
